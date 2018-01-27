5437d18ff4778a1305a0be62ef869d61e054b1fe
Mark Blanford <markytorture@hotmail.com>
2018-01-27 17:07:11 +0000

    Added submit button to AddTask Activity

--- app/src/main/java/example/codeclan/com/prioritease/AddTaskActivity.java
+++ app/src/main/java/example/codeclan/com/prioritease/AddTaskActivity.java
@@ -1,7 +1,9 @@
 package example.codeclan.com.prioritease;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddTaskActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+    }
+
+    public void onSubmitButtonClicked(View view){
+        Intent intent = new Intent(this, ViewListActivity.class);
+        startActivity(intent);
+        //include a Toast
     }
 }
