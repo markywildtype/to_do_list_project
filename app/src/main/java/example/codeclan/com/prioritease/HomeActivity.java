0e3a1730aa43db21d6178bacff2f8106ecfe76b1
Mark Blanford <markytorture@hotmail.com>
2018-01-27 16:09:41 +0000

    Main activity created in Android

--- app/src/main/java/example/codeclan/com/prioritease/HomeActivity.java
+++ app/src/main/java/example/codeclan/com/prioritease/HomeActivity.java
@@ -2,12 +2,25 @@ package example.codeclan.com.prioritease;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button viewListButton;
+    Button newTaskButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        viewListButton = findViewById(R.id.view_list_button);
+        newTaskButton = findViewById(R.id.new_task_button);
     }
+
+    public void onAddTaskButtonClicked(){
+
+    }
+
+
 }
