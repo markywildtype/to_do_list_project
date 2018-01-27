46be4225fa2fd0aae83d45c4528d346d587e0d68
Mark Blanford <markytorture@hotmail.com>
2018-01-27 21:11:46 +0000

    FloatingActionButton added to ViewList - not working yet

--- app/src/main/java/example/codeclan/com/prioritease/ActivityMenu.java
+++
@@ -1,8 +0,0 @@
-package example.codeclan.com.prioritease;
-
-/**
- * Created by user on 27/01/2018.
- */
-
-public class ActivityMenu {
-}
--- app/src/main/java/example/codeclan/com/prioritease/ViewListActivity2.java
+++ app/src/main/java/example/codeclan/com/prioritease/ViewListActivity2.java
@@ -1,35 +1,34 @@
 package example.codeclan.com.prioritease;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-public class ViewListActivity extends AppCompatActivity {
+public class ViewListActivity2 extends AppCompatActivity {
 
     FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_list);
+        setContentView(R.layout.activity_view_list2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener(){
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(ViewListActivity.this, AddTaskActivity.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(ViewListActivity2.this, AddTaskActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 
     @Override
@@ -52,4 +51,5 @@ public class ViewListActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
