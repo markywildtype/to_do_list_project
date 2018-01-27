d04af1424176f8f5a4916cc180b8ea3cc60273c0
Mark Blanford <markytorture@hotmail.com>
2018-01-27 16:22:27 +0000

    added basic empty views for AddTask and ViewList

--- app/src/main/java/example/codeclan/com/prioritease/HomeActivity.java
+++ app/src/main/java/example/codeclan/com/prioritease/HomeActivity.java
@@ -1,7 +1,9 @@
 package example.codeclan.com.prioritease;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
@@ -18,8 +20,14 @@ public class HomeActivity extends AppCompatActivity {
         newTaskButton = findViewById(R.id.new_task_button);
     }
 
-    public void onAddTaskButtonClicked(){
+    public void onAddTaskButtonClicked(View button){
+        Intent intent = new Intent(this, AddTaskActivity.class);
+        startActivity(intent);
+    }
 
+    public void onViewListButtonClicked(View button){
+        Intent intent = new Intent(this, ViewListActivity.class);
+        startActivity(intent);
     }
 
 
