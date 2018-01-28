6e9493f266deeffd26c40f3e9db8692195fefc5d
Mark Blanford <markytorture@hotmail.com>
2018-01-28 11:11:58 +0000

    FAB working

--- app/src/main/java/example/codeclan/com/prioritease/ViewListActivity.java
+++ app/src/main/java/example/codeclan/com/prioritease/ViewListActivity.java
@@ -18,10 +18,8 @@ public class ViewListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
