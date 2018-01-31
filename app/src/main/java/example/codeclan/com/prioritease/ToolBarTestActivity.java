429a505e25266e2e7225267344636918bea88e0e
Mark Blanford <markytorture@hotmail.com>
2018-01-31 20:37:20 +0000

    icons, logos and further styling added

---
+++ app/src/main/java/example/codeclan/com/prioritease/ToolBarTestActivity.java
@@ -0,0 +1,17 @@
+package example.codeclan.com.prioritease;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class ToolBarTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar_test);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
